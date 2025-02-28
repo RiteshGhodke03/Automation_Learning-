ee01a84090fb51cc473b105662602e3b47200a9b
Ritesh Ghodke <Riteshrg03@gmail.com>
2025-02-28 16:37:03 +0530

    selenium

---
+++ Python_Selenium/Lab001_First program.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+# Correct path with escaped backslashes
+service = Service(r"C:\Users\Ritesh Ghodke\Downloads\chrome-win64 (1)\chrome-win64\chrome.exe")
+
+driver = webdriver.Chrome(service=service)
+
+# Open Google
+driver.get("https://www.google.com")
+
+# Print page title
+print("Page Title:", driver.title)
+
+# Close browser
+driver.quit()
